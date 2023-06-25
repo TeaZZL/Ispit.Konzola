9d7a08f4d6c3fc305b4ccdfad7ff00015229a809
TeaZZL <tea.zlopasa@gmail.com>
2023-06-25 12:44:17 +0200

    Add project files.

---
+++ Ispit.Konzola/PametniTelefon.cs
@@ -0,0 +1,73 @@
+﻿using Ispit.Konzola.Sucelja;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ispit.Konzola
+{
+    public class PametniTelefon : IPametniTelefon, ITelefon
+    {
+        public string Model { get; private set; }
+        public PametniTelefon(string model)
+        {
+            Model = model;
+        }
+
+        public string Surfanje(string url)
+        {
+            if (ProvjeriAkoJeValidanUrl(url))
+            {
+                Console.WriteLine("URL je ispravan i možete surfati na njemu! ");
+                return "OK";
+            }
+            else
+            {
+                Console.WriteLine("URL nije ispravan! ");
+                return "NOT OK";
+            }
+
+        }
+
+        public string Poziv(string telefonski_broj)
+        {
+            if (ProvjeriAkoJeValidanBroj(telefonski_broj))
+            {
+                Console.WriteLine("Telefonski broj je ispravan i možete ga zvati! ");
+                return "OK";
+            }
+            else
+            {
+                Console.WriteLine("Telefonski broj nije ispravan! ");
+                return "NOT OK";
+            }
+        }
+
+        public bool ProvjeriAkoJeValidanUrl(string url)
+        {
+            foreach (char c in url)
+            {
+                if (c >= '0' && c <= '9')
+                {
+                    return false;
+                }
+            }
+            return true;
+
+        }
+
+        public bool ProvjeriAkoJeValidanBroj(string telefonski_broj)
+        {
+
+            foreach (char c in telefonski_broj)
+            {
+                if (c < '0' || c > '9')
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Ispit.Konzola/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ispit.Konzola
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            PametniTelefon pametniTelefon = new PametniTelefon("Xiaomi");
+            TestirajTelefon(pametniTelefon);
+        }
+
+        private static void TestirajTelefon(PametniTelefon telefon)
+        {
+            Console.Write("Unesite telefonski broj bez razmaka: ");
+            var telBroj = Console.ReadLine();
+
+            Console.Write("Unesite URL: ");
+            var url = Console.ReadLine();
+
+            telefon.Poziv(telBroj);
+            telefon.Surfanje(url);
+        }
+    }
+}
